915e7b30ab2e80aa0a1242991ea129e15645e262
Jonathan Dempsey <jonathanedempsey@gmail.com>
2021-12-22 12:26:52 +0000

    Intersection Observer working with infinite scrolling

--- script.js
+++ script.js
@@ -25,3 +25,43 @@ const observer = new IntersectionObserver(entries => {
 cards.forEach(card => {
     observer.observe(card);
 })
+
+
+/**
+ * This observer will only select the last card in our list,
+ * and load in new cards when we reach the last card
+ */
+const lastCardObserver = new IntersectionObserver(entries => {
+    const lastCard = entries[0];
+
+    // If our last card is not intersecting (becoming visible), don't do anything
+    if (!lastCard.isIntersecting) return;
+
+    // If the last card is intersecting (becoming visible), then load a new set of cards
+    loadNewCards();
+
+    // Unobserve the last card since we have loaded in a bunch of new cards,
+    // meaning there's a new last card.
+    lastCardObserver.unobserve(lastCard.target);
+
+    // Observe our new last card
+    lastCardObserver.observe(document.querySelector('.card:last-child'));
+}, {
+    rootMargin: "100px" // ensures new cards get loaded in 100px before the end of page
+});
+lastCardObserver.observe(document.querySelector('.card:last-child'));
+
+// In a real world example you would do a fetch to an API,
+// but here we just create 10 new static card elements and add them to our observer
+cardContainer = document.querySelector('.card-container');
+function loadNewCards() {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement('div');
+        card.textContent = "New Card";
+        card.classList.add('card');
+        observer.observe(card);
+        cardContainer.append(card);
+    }
+}
+
+
